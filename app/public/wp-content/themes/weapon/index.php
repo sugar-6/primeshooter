1f23bd26664886a1cdf4440dee9c6c7352fb74aa
sugar-6 <haru19970125+4@gmail.com>
2022-09-05 20:11:08 +0900

    weaponのテーマの作成など

---
+++ app/public/wp-content/themes/weapon/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>Prime Shooter</title>
+<meta name="description" content="プライムシューターのファンサイトです">
+<!-- <meta name="keywords" content="サイトキーワードを,で区切って入力"> -->
+<?php wp_head(); ?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+</head>
+
+<body>
+  <section class="mv">
+
+    <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/mv.jpeg" alt=""> -->
+    <p></p>
+  </section>
+</body>
+</html>
+
